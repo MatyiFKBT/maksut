040294436b6900d13b18ae9a703d230967690ede
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2025-10-26 21:33:59 +0100

    chore: tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import { vercelToolbar } from "@vercel/toolbar/plugins/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [sveltekit(), vercelToolbar()],
+  plugins: [tailwindcss(), sveltekit(), vercelToolbar()],
   optimizeDeps: {
     exclude: ["@tanstack/svelte-query"],
   },
